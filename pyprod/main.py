68bc94a95512f6cbd01a02740458e76fe63e2c84
maksimnickalayevich <maksim.nickalayevich@deliverect.com>
2024-01-25 14:21:22 +0100

    move app to main

--- pyprod/main.py
+++ pyprod/main.py
@@ -1,17 +1,24 @@
+# 3rd party imports
 from fastapi import FastAPI
 import uvicorn
-
+import sys
+# Internal imports
 from database.db import Database
 
-app: FastAPI = FastAPI()
+# Set global setting to ensure no __pycache__ will be created
+sys.dont_write_bytecode = True
 
-@app.get("/")
-async def hello() -> dict:
-    return {"message":  "Hello from FastAPI"}
+
+def main() -> None:
+    app: FastAPI = FastAPI(title="PyProd")
+    
+    # TODO: Make database object global, to ensure it is accessible
+    # in different routes
+    db: Database = Database()
+
+    # TODO: Ensure the app is global and add different 
+    # blueprints for different urls
+    uvicorn.run(app=app, host="0.0.0.0", port=5050)
 
 if __name__ == "__main__":
-    import sys
-    sys.dont_write_bytecode = True
-    
-    db: Database = Database()
-    uvicorn.run(app=app, host="0.0.0.0", port=5050)
+    main()
